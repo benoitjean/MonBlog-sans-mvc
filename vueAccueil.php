c68d491b8336de18d50b5fc824aaaaccb725d638
benoitjean <ruelle.benoitjean@gmail.com>
2017-09-19 14:42:09 +0200

    3

--- vueAccueil.php
+++ vueAccueil.php
@@ -6,15 +6,26 @@
         <title>Mon Blog</title>
 </head>
 <body>
-  ...
-    <div id="contenu">
+<div id="global">
+            <header>
+                <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
+                <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
+            </header>
+                <div id="contenu">
       <?php foreach ($billets as $billet): ?>
         <article>
-          ...
+       <header>
+                            <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+                            <time><?= $billet['date'] ?></time>
+                        </header>
+                        <p><?= $billet['contenu'] ?></p>
         </article>
         <hr />
       <?php endforeach; ?> 
     </div> <!-- #contenu -->
-  ...
+<footer id="piedBlog">
+                Blog réalisé avec PHP, HTML5 et CSS.
+            </footer>
+        </div> <!-- #global -->
 </body>
 </html>
