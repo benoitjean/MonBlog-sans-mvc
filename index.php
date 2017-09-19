e316a7249420ddfac5ef0a30bb783a18801a15c4
benoitjean <ruelle.benoitjean@gmail.com>
2017-09-19 16:16:29 +0200

    6486

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
                 	require 'vueAccueil.php';
                 }
                 catch (Exception $e) {
-                	echo '<html><body>Erreur ! ' . $e->getMessage() . '</body></html>';
+                	$msgErreur = $e->getMessage();
+                	require 'vueErreur.php';
                 }
