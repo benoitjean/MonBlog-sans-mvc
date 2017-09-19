5950b6261f88b0bc8c3f416fdac02b99f07ab909
benoitjean <ruelle.benoitjean@gmail.com>
2017-09-19 16:09:13 +0200

    7

--- Modele.php
+++ Modele.php
@@ -1,11 +1,16 @@
 <?php
 
-// Renvoie la liste de tous les billets, triés par identifiant décroissant
+// Renvoie la liste de tous les billets, triï¿½s par identifiant dï¿½croissant
 function getBillets() {
-	$bdd = new PDO('mysql:host=localhost;dbname=miseenplacemvc;charset=utf8',
-                        'ts2', 'ts2');
+	$bdd = getBdd();
 	$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
 			. ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
 			. ' order by BIL_ID desc');
 	return $billets;
+}
+
+function getBdd() {
+	$bdd = new PDO('mysql:host=localhost;dbname=miseenplacemvc;charset=utf8',
+			'ts2', 'ts2');
+	return $bdd;
 }
